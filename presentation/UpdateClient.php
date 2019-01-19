55e8b87429faeccc27bb1f360612e043f550b668
Luis Castillo <lcastillo@outcoding.com>
2019-01-19 12:24:10 -0600

    Cam in update client

--- presentation/UpdateClient.php
+++ presentation/UpdateClient.php
@@ -149,13 +149,16 @@ include './reusable/Footer.php';
     };
     function startCam() {
         $('#photo').hide();
-        navigator.getUserMedia({'audio': false, 'video': true}, function (streamVideo) {
-            datosVideo.StreamVideo = streamVideo;
-            datosVideo.url = window.URL.createObjectURL(streamVideo);
-            jQuery('#cam').attr('src', datosVideo.url);
-        }, function () {
-            alertify.error("No fue posible obtener acceso a la cámara.");
-        });
+        var video = document.getElementById('cam');
+        
+        // Get access to the camera!
+        if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+            // Not adding `{ audio: true }` since we only want video now
+            navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
+                video.srcObject = stream;
+                video.play();
+            });
+        }
     }
 
     jQuery(document).ready(function () {
